8094c5edbc0a5ed362672eb1f59bbe7f769357ea
TSK2302 <sayant2001@gmail.com>
2025-08-21 01:31:12 +0530

    added mobile menu toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,16 @@
 import 'boxicons/css/boxicons.min.css'
 const Header = () => {
+
+    const toggleMobileMenu = () => {
+        const mobileMenu = document.getElementById('mobileMenu')
+
+        if (mobileMenu.classList.contains('hidden')){
+            mobileMenu.classList.remove('hidden');
+        } else {
+            mobileMenu.classList.add('hidden')
+        }
+    }
+
   return (
     <header className="py-1 px-7 flex justify-between items-center fixed top-0 z-50 w-full border-b-[0.3px] border-[#babaff]">
         {/*Left section */}
@@ -31,6 +42,40 @@ const Header = () => {
                 <i className='bx bx-store'></i>Shop
             </a>
         </nav>
+
+        {/* mobile menu bar */}
+        <button onClick={toggleMobileMenu} className='text-3xl p-2 md:hidden'>
+            <i className='bx bx-menu'></i>
+        </button>
+
+        {/* mobile menu hidden by default */}
+        <div id="mobileMenu" className='hidden fixed top-14 right-0 left-0 bg-black p-5 md:hidden'>
+            <nav className='flex flex-col gap-4 items-center'>
+                <a href="#" className="relative py-1 text-lg hover:text-purple-300 transition-colors duration-300 after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-purple-400 after:left-0 after:bottom-0 after:transition-all hover:after:w-full text-nowrap">
+                <i className='bx bx-user-circle'></i>Avatar
+                </a>
+                <a href="#" className="relative py-1 text-lg hover:text-purple-300 transition-colors duration-300 after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-purple-400 after:left-0 after:bottom-0 after:transition-all hover:after:w-full text-nowrap">
+                    <i className='bx bx-diamond'></i>Arena
+                </a>
+                <a href="#" className="relative py-1 text-lg hover:text-purple-300 transition-colors duration-300 after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-purple-400 after:left-0 after:bottom-0 after:transition-all hover:after:w-full text-nowrap">
+                    <i className='bx bx-chevrons-up'></i>Beyond
+                </a>
+                <a href="#" className="relative py-1 text-lg hover:text-purple-300 transition-colors duration-300 after:content-[''] after:absolute after:w-0 after:h-0.5 after:bg-purple-400 after:left-0 after:bottom-0 after:transition-all hover:after:w-full text-nowrap">
+                    <i className='bx bx-store'></i>Shop
+                </a>
+            </nav>
+
+            <div className='flex flex-col gap-3 w-full mt-4'>
+                <button className='bg-purple-700 py-2 rounded'>
+                    Play Now
+                </button>
+
+                <button className='bg-gray-500 py-2 rounded'>
+                    NFT Store
+                </button>
+            </div>
+
+        </div>
 
     </header>
   )
